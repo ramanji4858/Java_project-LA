aecd87ad27fb10dc0ab81548211826375c33d99b
ramanji4858 <36157775+ramanji4858@users.noreply.github.com>
2018-03-27 12:35:11 +0530

    copy from linux

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,21 +1,18 @@
-Public class Rectangle {
-Public int length;
-Public int width;
+public class Rectangle {
+  public int length;
+  public int width;
 
-Public Rectangle(int length , int width) {
-this.length = length;
-this.width = width;
-}
-
-Public int getArea()  {
-return length * width;
-
-}
-
-Public int getPerimeter() {
-return 2 * (length + width);
-
+  public Rectangle(int length, int width) {
+    this.length = length;
+    this.width = width;
   }
- 
+
+  public int getArea() {
+    return length * width;
+  }
+
+  public int getPerimeter() {
+    return 2 * (length + width);
+  }
 }
 
